4323347a8cc2e3b463294baef66dd806ca191396
andresbenji <116680825+andresbenji@users.noreply.github.com>
2023-07-18 00:45:44 -0400

    fixed server bug

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 //Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -33,9 +30,7 @@ app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   //set static folder
   app.use(express.static(path.join(__dirname, "/frontend/build")));
