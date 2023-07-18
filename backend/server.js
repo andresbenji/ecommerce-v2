9855e852a229d3f9ed7690fbbfcdc4632580fff4
andresbenji <116680825+andresbenji@users.noreply.github.com>
2023-07-17 23:54:31 -0400

    server bug

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
   //set static folder
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
@@ -44,6 +45,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
